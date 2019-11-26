6c18b1ecb088d80f6a38ed90e8a6d9d8a76402ee
Mkaychuks <sachufusi@gmail.com>
2019-11-26 14:58:24 +0100

    Added styling and queryset

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.utils import timezone
 
 from .models import Post
 
+
 # function to handle post_list
 def post_list(request):
-    context = {}
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()
+    ).order_by('published_date')
+    context = {
+        'posts': posts
+    }
     template_name = 'blog/post_list.html'
     return render(request, template_name, context)
