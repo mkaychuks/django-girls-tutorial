54462ffffa4d981367f1d91e817b5f06faf47cb9
Mkaychuks <sachufusi@gmail.com>
2019-11-26 19:23:44 +0100

    Added "post_detail" view and urls, using the get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.utils import timezone
 
@@ -9,9 +9,20 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()
-    ).order_by('published_date')
+    ).order_by('published_date') # first filters the posts using the negative published_date
+                                 # and then orders it accordingly...
     context = {
         'posts': posts
     }
     template_name = 'blog/post_list.html'
+    return render(request, template_name, context)
+
+
+# handles the post detail
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # gets a specific post using the PrimaryKey
+    context = {
+        'post': post
+    }
+    template_name = 'blog/post_detail.html'
     return render(request, template_name, context)
